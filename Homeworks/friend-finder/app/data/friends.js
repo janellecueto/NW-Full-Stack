e377b36cabd4d12dd62c87983dee652a8ff933fd
Janelle Cueto <janellecueto@gmail.com>
2019-07-23 20:26:15 -0500

    updated

--- Homeworks/friend-finder/app/data/friends.js
+++ Homeworks/friend-finder/app/data/friends.js
@@ -4,6 +4,11 @@ var friends = [
         photo: "http://picture-cdn.wheretoget.it/7tqdfu-l-610x610-home+accessory-duck-plushie-plushies-soft.jpg",
         scores: [4,4,3,2,4,1,3,2,5,3]
     },
+    {
+        name: "Joey",
+        photo: "https://i0.wp.com/butterwithasideofbread.com/wp-content/uploads/2012/07/Easiest-Best-Homemade-Bread.BSB_.IMG_6014.jpg?fit=775%2C517&ssl=1",
+        scores: [3,4,3,2,4,1,3,3,5,3]
+    }
 ]
 
 module.exports = friends;
--- Homeworks/friend-finder/app/routing/apiRoutes.js
+++ Homeworks/friend-finder/app/routing/apiRoutes.js
@@ -5,6 +5,41 @@ module.exports = function(app){
         res.json(friendList);
     })
     app.post("/api/friends", function(req, res){
-        //add new person to friends
+        
+        //find best match
+        //  req.body.scores is array of new friend
+        var index = 0;
+        var lowesti = 0;
+        var lowest = 99;
+        var curr = req.body;
+        // for(friend in friendList){
+        //     console.log("friend?: ",friend);
+        //     var diff = 0;
+        //     for(var i = 0; i < 10; i++){
+        //         diff += Math.abs(parseInt(curr.scores[i])-parseInt(friend["scores"][i]));
+        //     }
+        //     if(diff < lowest){
+        //         lowest = diff;
+        //         lowesti = index;
+        //     }
+        //     index++;
+        // }
+        friendList.forEach(function(friend){
+            // console.log(friend);
+            // console.log(friend.scores);
+            var diff = 0;
+            for(var i = 0; i < 10; i++){
+                diff += Math.abs(parseInt(curr.scores[i])-parseInt(friend["scores"][i]));
+            }
+            if(diff < lowest){
+                lowest = diff;
+                lowesti = index;
+            }
+            index++;
+        })
+        console.log("lowest diff:",lowest);
+        friendList.push(curr);
+        //return json of best match
+        res.json(friendList[lowesti]);
     });
 }
