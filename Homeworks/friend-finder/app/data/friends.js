f1b4a3e98d63cfb06ace13069060cefdcdfd1b2c
Janelle Cueto <janellecueto@gmail.com>
2019-07-23 20:31:54 -0500

    minor updates

--- Homeworks/friend-finder/app/data/friends.js
+++ Homeworks/friend-finder/app/data/friends.js
@@ -3,11 +3,6 @@ var friends = [
         name: "Janelle",
         photo: "http://picture-cdn.wheretoget.it/7tqdfu-l-610x610-home+accessory-duck-plushie-plushies-soft.jpg",
         scores: [4,4,3,2,4,1,3,2,5,3]
-    },
-    {
-        name: "Joey",
-        photo: "https://i0.wp.com/butterwithasideofbread.com/wp-content/uploads/2012/07/Easiest-Best-Homemade-Bread.BSB_.IMG_6014.jpg?fit=775%2C517&ssl=1",
-        scores: [3,4,3,2,4,1,3,3,5,3]
     }
 ]
 
