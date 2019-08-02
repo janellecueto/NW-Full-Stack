a5edcef33d8d8f8556652bd50de80e3952ed8d57
Janelle Cueto <janellecueto@gmail.com>
2019-08-01 21:23:36 -0500

    adding eat-da-burger hw

---
+++ Homeworks/eat-da-burger/controllers/burgerController.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var router = express.Router();
+
+var burger = require("../models/burgers.js");
+
+router.get("/", function(req, res){
+    burger.all(function(data){
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+})
+
+router.post("/api/burgers", function(req, res){
+    burger.create(["name"], [req.body.name], function(result){
+        res.json({id: result.insertId});
+    })
+})
+
+router.put("/api/burgers/:id", function(req, res){
+    var condition = "id = "+req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devour: req.body.devour
+    }, condition, function(result){
+        if(result.changedRows == 0){
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    })
+})
+
+router.delete("api/burgers/:id", function(req, res){
+
+})
+
+module.exports = router;
