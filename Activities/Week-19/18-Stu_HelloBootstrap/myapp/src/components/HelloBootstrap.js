7d4e97956bb05130e06abf78f295760c2c77455f
Janelle Cueto <janellecueto@gmail.com>
2019-08-27 20:46:04 -0500

    updated

--- Activities/Week-19/18-Stu_HelloBootstrap/myapp/src/components/HelloBootstrap.js
+++ Activities/Week-19/18-Stu_HelloBootstrap/myapp/src/components/HelloBootstrap.js
@@ -1,20 +1,21 @@
 import React from "react"
 
-function helloBootstrap(){
+function HelloBootstrap(){
     return(
         <div>
             <nav class="navbar"></nav>
             <div class="jumbotron">
                 <h1>I am a jumbotron :)</h1>
             </div>
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="..." alt="Card image cap" />
+            <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    
                 </div>
             </div>
         </div>
     );
 }
+
+export default HelloBootstrap;
