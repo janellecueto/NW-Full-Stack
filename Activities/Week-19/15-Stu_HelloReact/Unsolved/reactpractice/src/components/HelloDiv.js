ef8b05a8208bda64a85413be906f6e91366c002f
Janelle Cueto <janellecueto@gmail.com>
2019-08-27 19:00:05 -0500

    adding week 19 activities: react

---
+++ Activities/Week-19/15-Stu_HelloReact/Unsolved/reactpractice/src/components/HelloDiv.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function HelloDiv() {
+  var retStr = 
+    <div>
+      <h1>Hello! My name is Janelle</h1>
+      <ul>
+        <li>Chocolate</li>
+        <li>Mac n cheese</li>
+        <li>Pugs</li>
+      </ul>
+    </div>;
+  return retStr;
+}
+
+export default HelloDiv;
