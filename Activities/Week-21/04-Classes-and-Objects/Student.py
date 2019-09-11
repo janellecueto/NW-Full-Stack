6166e9e2c1bbfb773ccbecc7b9af0483fdb48d18
Janelle Cueto <janellecueto@gmail.com>
2019-09-10 19:57:30 -0500

    adding Student class starter

---
+++ Activities/Week-21/04-Classes-and-Objects/Student.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self, first_name, last_name, middle_initial, address, email, phone_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.middle_initial = middle_initial
+        self.address = address
+        self.email = email
+        self.phone_number = phone_number
+
+    
