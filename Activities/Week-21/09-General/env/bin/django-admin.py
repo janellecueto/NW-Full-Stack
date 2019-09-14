1761174ef82b67907db2d2fce03059c2b6efaf13
Janelle Cueto <janellecueto@gmail.com>
2019-09-14 13:45:48 -0500

    removing env

--- Activities/Week-21/09-General/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/janelle/coding/NW-Full-Stack/Activities/Week-21/09-General/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
